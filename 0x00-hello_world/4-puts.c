a05c1adede2667b6bbcfb783ae27fda1d035e97c
Goldfishater <bdmasinde@gmail.com>
2023-07-13 06:57:01 -0700

    four puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
-#!/bin/bash
-#include <stdio.h>
-
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+/**
+* main - A program that prints a line with puts function
+* Return: 0 (sucess)
+*/
+int main(void)
+{
+puts(""Programming is like building a multilingual puzzle");
+return (0);
 }
