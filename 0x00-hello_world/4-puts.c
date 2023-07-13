d60534df9311fa752e5e9764abf5e47b187ec1b5
Goldfishater <bdmasinde@gmail.com>
2023-07-13 07:40:18 -0700

    4 p fin

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 /**
-* main - A program that prints a  line with puts function
-* Return: 0 (sucess)
-*/
+ * main - A program that prints a line with puts function
+ * Return: 0 (succss)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle\n");
 return (0);
 }
