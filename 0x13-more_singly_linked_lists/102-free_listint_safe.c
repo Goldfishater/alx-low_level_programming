59d0e0ede90c3e69005ffcedd561d979cbf6d9ee
Goldfishater <bdmasinde@gmail.com>
2023-08-29 12:34:34 -0700

    l

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ *free_listint_safe -this is responsible for freeing a linked list
+ * This function traverses the linked list and frees each node.
+ * It also checks for a NULL pointer and returns an error if one is found.
+ * @h: Pointer to the first node in the linked list.
+ * Returns: The number of elements in the freed list.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (len);
+}
