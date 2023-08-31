92f8575488a57f104387c587aa18c329a7433661
Dev Abiud <136119623+Goldfishater@users.noreply.github.com>
2023-08-31 22:30:38 +0300

    gg

--- 0x14-bit_manipulation/1-main.c
+++ 0x14-bit_manipulation/1-main.c
@@ -2,25 +2,6 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
-int main(void)
-{
-	    print_binary(0);
-	        printf("\n");
-		    print_binary(1);
-		        printf("\n");
-			    print_binary(98);
-			        printf("\n");
-				    print_binary(1024);
-				        printf("\n");
-					    print_binary((1 << 10) + 1);
-					        printf("\n");
-						    return (0);
-=======
  * main - check the code
  *
  * Return: Always 0.
@@ -38,5 +19,4 @@ int main(void)
     print_binary((1 << 10) + 1);
     printf("\n");
     return (0);
->>>>>>> 0f266081f4242e3810eca900eb483082afc572a9
 }
